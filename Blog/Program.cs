bee529adadd1139ab5d42ae992e38c20aa7193b0
lgc2 <lgustavoccarvalho@gmail.com>
2024-02-20 20:44:10 -0300

    feat(Blog): implements Create, Update and Delete

--- Blog/Program.cs
+++ Blog/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Blog.Data;
+using Blog.Models;
 
 namespace Blog
 {
@@ -6,7 +7,25 @@ namespace Blog
     {
         static void Main(string[] args)
         {
-            var ctx = new BlogDataContext();
+            using (var context = new BlogDataContext())
+            {
+                // CREATE
+                // var tag = new Tag { Name = "ASP.NET", Slug = "aspnet" };
+                // context.Tags.Add(tag);
+
+                // UPDATE
+                // var tag = context.Tags.FirstOrDefault(x => x.Name == "ASP.NET");
+                // tag.Name = ".NET";
+                // tag.Slug = "dotnet";
+                // context.Update(tag);
+
+                // DELETE
+                // var tag = context.Tags.FirstOrDefault(x => x.Id == 7);
+                // context.Remove(tag);
+
+                // context.SaveChanges();
+
+            }
         }
     }
 }
