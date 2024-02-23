c71306100faf0d24bfcdbec72651369c193981ae
lgc2 <lgustavoccarvalho@gmail.com>
2024-02-23 20:00:44 -0300

    chore(Modulo2): implements inserts into User, Category and Post tables

--- Modulo2/Data/BlogDataContext.cs
+++ Modulo2/Data/BlogDataContext.cs
@@ -5,6 +5,12 @@ namespace Blog.Data
 {
     public class BlogDataContext : DbContext
     {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<User> Users { get; set; }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlServer(@"Server=127.0.0.1,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$;TrustServerCertificate=true");
     }
 }
--- Modulo2/Program.cs
+++ Modulo2/Program.cs
@@ -8,42 +8,38 @@ namespace Blog
     {
         static void Main(string[] args)
         {
-            using (var context = new BlogDataContext())
+            using var context = new BlogDataContext();
+
+            var user = new User
             {
-                // CREATE
-                // var tag = new Tag { Name = ".NET", Slug = "dotnet" };
-                // context.Tags.Add(tag);
+                Name = "André Baltieri",
+                Slug = "andrebaltieri",
+                Email = "andre@balta.io",
+                Bio = "11x Microsoft MVP",
+                Image = "https://balta.io",
+                PasswordHash = "09238423aosjdaso"
+            };
 
-                // UPDATE --> não utilizar AsNoTracking
-                // var tag = context.Tags.FirstOrDefault(x => x.Name == "ASP.NET");
-                // tag.Name = ".NET";
-                // tag.Slug = "dotnet";
-                // context.Update(tag);
+            var category = new Category
+            {
+                Name = "Banco de dados",
+                Slug = "banco-de-dados"
+            };
 
-                // DELETE --> não utilizar AsNoTracking
-                // var tag = context.Tags.FirstOrDefault(x => x.Id == 7);
-                // context.Remove(tag);
+            var post = new Post
+            {
+                Author = user,
+                Category = category,
+                Body = "<p>Hello World!!!</p>",
+                Slug = "comecando-com-ef-core",
+                Summary = "Neste artigo vamos aprender EF Core",
+                Title = "Começando com EF Core",
+                CreateDate = DateTime.UtcNow.AddHours(-3),
+                LastUpdateDate = DateTime.UtcNow.AddHours(-3)
+            };
 
-                // context.SaveChanges();
-
-                // SELECT
-                // var tags = context.Tags; // só é uma referência, não foi no banco ainda
-                // var tags = context
-                // .Tags
-                // .AsNoTracking() // desabilita o tracking para a consulta, ou seja, não traz metadados (ganho de performance)
-                // .ToList(); // com o ToList() a query foi de fato executada. O ToList() deve ser colocado sempre no final da query
-                // foreach (var item in tags)
-                // {
-                //     Console.WriteLine(item.Name);
-                // }
-
-                var tag = context
-                    .Tags
-                    .AsNoTracking()
-                    .FirstOrDefault(x => x.Id == 8);
-
-                Console.WriteLine(tag?.Name);
-            }
+            context.Posts.Add(post);
+            context.SaveChanges();
         }
     }
 }
